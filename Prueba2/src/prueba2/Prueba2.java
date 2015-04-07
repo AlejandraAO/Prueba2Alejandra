180a6981a6cddbfd5023840c3bd20ea880bcfe6f
Luis <Luis@Luis>
2015-04-07 11:13:56 -0600

    Mensaje de Hola en la clase Prueba2

--- Prueba2/src/prueba2/Prueba2.java
+++ Prueba2/src/prueba2/Prueba2.java
@@ -5,6 +5,8 @@
  */
 package prueba2;
 
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author usuario
@@ -16,6 +18,7 @@ public class Prueba2 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        JOptionPane.showMessageDialog(null,"Hola gente");
     }
     
 }
